daac8875cfe4e187ed51353b30def4db8f2143e9
sureshk@aristanetworks.com <sureshk@arista.com>
2021-03-09 09:13:53 -0500

    removed logger_cmd

--- run-logger.py
+++ run-logger.py
@@ -8,12 +8,11 @@ switch=Server("http://admin:admin@172.30.154.65/command-api")
 
 
 def exec_and_forget_cmd(CMD):
-    loggercmd='logger 1 write'
     switch.runCmds( 1, [CMD+'&'])
 
 def main():
     UID= input("Enter UID ")
-    CMD='bash timeout 1000 python /mnt/flash/test.py ' + UID + ' "show version"'
+    CMD='bash timeout 1000 python /mnt/flash/test.py ' + UID + ' "show tech-support"'
     print (CMD)
     exec_and_forget_cmd(CMD)
 
